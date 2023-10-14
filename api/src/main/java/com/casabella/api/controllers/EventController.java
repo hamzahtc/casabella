0de1099d4e41bf049db3b4f05e41eb8b87e80c36
hamzahtc <hamzahatoch04@gmail.com>
2023-10-14 20:31:18 +0100

    update endpoint

--- api/src/main/java/com/casabella/api/controllers/EventController.java
+++ api/src/main/java/com/casabella/api/controllers/EventController.java
@@ -3,11 +3,13 @@ package com.casabella.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EventController {
 
     @GetMapping("/events")
-    public String getAllEvents() {
-        return "Casa Bella";
+    public List<String> getAllEvents() {
+        return List.of("Amine", "Hamza", "Oussama", "Ibrahim");
     }
 }
---
+++ api/src/main/java/com/casabella/api/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.casabella.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().permitAll();
+        return http.build();
+    }
+
+
+}
