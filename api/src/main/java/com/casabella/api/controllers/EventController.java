5edb8d091f07106c226a5b11e3b66e264b9bb822
hamzahtc <hamzahatoch04@gmail.com>
2023-10-19 22:59:06 +0100

    remove data and security

--- api/src/main/java/com/casabella/api/controllers/EventController.java
+++ api/src/main/java/com/casabella/api/controllers/EventController.java
@@ -1,28 +1,15 @@
 package com.casabella.api.controllers;
 
 import com.casabella.api.models.Event;
-import com.casabella.api.repositories.EventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class EventController {
-
-    @Autowired
-    EventRepository eventRepository;
-
     @GetMapping("/events")
     public List<Event> getAllEvents() {
-        return eventRepository.findAll();
-    }
-
-    @PostMapping("/events")
-    public Event getAllEvents(@RequestBody Event event) {
-        return eventRepository.save(event);
+        return List.of(new Event(1, "Hamza"), new Event(2, "Amine"));
     }
 }
--- api/src/main/java/com/casabella/api/models/Event.java
+++ api/src/main/java/com/casabella/api/models/Event.java
@@ -1,24 +1,13 @@
 package com.casabella.api.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Event {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
-
-    public Event(String name) {
-        this.name = name;
-    }
 }
